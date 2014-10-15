68395d68ad2bf0570117c80a1b8978c3323419e1
Max White <mushishi78@gmail.com>
2014-10-15 18:16:22 +0100

    Guard against not having a payment method that has a require_card_numbers? method defined

--- app/models/spree/credit_card_decorator.rb
+++ app/models/spree/credit_card_decorator.rb
@@ -7,7 +7,10 @@ private
 
 	alias_method :__old_require_card_numbers?, :require_card_numbers?
  	def require_card_numbers?
- 		__old_require_card_numbers? && payment_method.require_card_numbers?
+ 		return false unless __old_require_card_numbers?
+ 		return true unless payment_method
+ 		return true unless payment_method.respond_to?(:require_card_numbers?)
+ 		payment_method.require_card_numbers?
  	end
 
 end
