6d723649943c305b71dba63531305181320fd72a
Max White <mushishi78@gmail.com>
2014-10-17 16:09:33 +0100

    if invalid? guard rather than unless checkout?

--- app/models/spree/gateway/paymill_bridge.rb
+++ app/models/spree/gateway/paymill_bridge.rb
@@ -39,7 +39,7 @@ module Spree
         end
 
         def create_profile(payment)
-            return unless payment.checkout?
+            return if payment.invalid?
             card = payment.source
             paymill_request do
                 card.paymill_client = Paymill::Client.find_or_create(payment.order.email, card.name).id
